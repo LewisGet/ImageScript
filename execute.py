2a4c1d692b7e4fd63b0ecf80d4e729114de89c77
Lewis Jang <lewis.aedes@gmail.com>
2016-12-19 19:12:05 +0800

    add actions helper

--- execute.py
+++ execute.py
@@ -1,17 +1,37 @@
 import training_data as td
+import helper as hp
+import numpy as np
 
-class taing_action:
+class training_action:
     action_name = None
+    correct_fps = 15
+    correct_target = "place"
+    correct_target_location = [0] * 3
     last_actions = []
     action_training = []
     execute_action = []
 
-    def __init__ (self, action_name, last_actions):
+    def __init__ (self, action_name, last_actions, action_location):
         self.action_name = action_name
         self.last_actions = last_actions
+        self.correct_target_location = action_location
 
     def load_traing_action (self):
         self.action_training = getattr(td, self.action_name)
 
-    def correct_action (self):
+    def correct_merge_last_actions (self):
         pass
+
+    def correct_location_by_target_location (self):
+        target_location = hp.xyz(self.get_training_target_location()).toArray()
+        correct_target_location = (target_location * -1) + hp.xyz(self.correct_target_location).toArray()
+
+        for fps, frames in enumerate(self.action_training):
+            location = hp.xyz(frames['location']).toArray()
+            location + correct_target_location
+            self.action_training[fps]['location'] = location
+
+    def get_training_target_location (self):
+        for frames in self.action_training:
+            if frames['block'] is self.correct_target:
+                return frames['blockLocation']
--- helper.py
+++ helper.py
@@ -21,3 +21,96 @@ class xyz:
 
     def toDict (self):
         return {'x': self.x, 'y': self.y, 'z': self.z}
+
+class training_data:
+    actions = []
+    actions_reverse = []
+    first_place_fps = None
+    first_place_location = None
+    first_place_location_array = None
+    last_place_fps = None
+    last_place_location = None
+    last_place_location_array = None
+
+    def __init__ (self, actions):
+        self.actions = actions
+        self.actions_reverse = actions[:]
+        self.actions_reverse.reverse()
+
+    def set_fps_location (self, fps, value):
+        self.actions[fps]['location'] = xyz(value)
+
+        return self
+
+    def get_fps_location (self, fps):
+        return xyz(self.actions[fps]['location'])
+
+    def get_fps_location_array (self, fps):
+        return self.get_fps_location().toArray()
+
+    def get_fps_block (self, fps):
+        return xyz(self.actions[fps]['block'])
+
+    def set_fps_block_location (self, fps, value):
+        self.actions[fps]['blockLocation'] = xyz(value)
+
+        return self
+
+    def get_fps_block_location (self, fps):
+        return xyz(self.actions[fps]['blockLocation'])
+
+    def get_fps_block_location_array (self, fps):
+        return self.get_fps_block_location().toArray()
+
+    def set_fps_direction (self, fps, value):
+        self.actions[fps]['direction'] = xyz(value)
+
+        return self
+
+    def get_fps_direction (self, fps):
+        return xyz(self.actions[fps]['direction'])
+
+    def get_fps_direction_array (self, fps):
+        return self.get_fps_direction().toArray()
+
+    def get_first_place (self):
+        if self.first_place_fps not None
+            return self.first_place_fps, self.first_place_location, self.first_place_location_array
+        
+        for fps, frame in enumerate(self.actions):
+            if self.get_fps_block(fps) is "place":
+                self.first_place_fps, self.first_place_location, self.first_place_location_array = fps, self.get_fps_block_location(fps), self.get_fps_block_location_array(fps)
+                return self.first_place_fps, self.first_place_location, self.first_place_location_array
+
+        return [None] * 3
+
+    def get_last_place (self):
+        if self.last_place_fps not None
+            return self.last_place_fps, self.last_place_location, self.last_place_location_array
+        
+        for fps, frame in enumerate(self.actions_reverse):
+            if self.get_fps_block(fps) is "place":
+                self.last_place_fps, self.last_place_location, self.last_place_location_array = fps, self.get_fps_block_location(fps), self.get_fps_block_location_array(fps)
+                return self.last_place_fps, self.last_place_location, self.last_place_location_array
+
+        return [None] * 3
+
+    def get_after_place_half_sec_fps (self):
+        value = []
+
+        placeFps, _, _ = self.get_last_place()
+
+        for fps in range(placeFps + 1, placeFps + 16):
+            value.append(self.actions[fps])
+
+        return value
+
+    def get_before_place_half_sec_fps (self):
+        value = []
+
+        placeFps, _, _ = self.get_first_place()
+
+        for fps in range(placeFps + 1, placeFps + 16):
+            value.append(self.actions[fps])
+
+        return value
